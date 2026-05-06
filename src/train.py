4a232933a51b12e8f1724d1b8e8042f635b4bc94
unknown <aishwaryagahukar@gmail.com>
2026-05-06 16:37:02 +0530

    remove xgboost

--- src/train.py
+++ src/train.py
@@ -16,7 +16,6 @@ from sklearn.ensemble import (
     GradientBoostingRegressor
 )
 
-from xgboost import XGBRegressor
 
 from data_preprocessing import (
     preprocess_data,
@@ -67,13 +66,6 @@ models = {
         random_state=42
     ),
 
-    "XGBoost": XGBRegressor(
-        n_estimators=100,
-        learning_rate=0.1,
-        max_depth=5,
-        random_state=42,
-        objective="reg:squarederror"
-    )
 }
 
 # =========================================================
